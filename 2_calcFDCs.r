23ac684b4c885d80a89455a7aa7a588eb0a2778f
Nipper <95218@CharlotteNC.gov>
2020-12-31 12:04:14 -0500

    Minor cleanup and explanation

--- 2_calcFDCs.r
+++ 2_calcFDCs.r
@@ -1,3 +1,6 @@
+## For matrix LDC stuff atleast, needed FDCs.  This is a rough implementatation
+## See format notes below.  Works with current workflows as is, but..
+
 rm(list=ls())
 
 ## List files present
@@ -28,13 +31,13 @@ resultDF <- as.data.frame(do.call("rbind", resres), stringsAsFactors=FALSE)
 names(resultDF)[1] <- "Site"
 names(resultDF)[2:1000] <- paste0("q", names(resultDF)[2:1000])
 
-## Name results
-## row.names(resultDF) <- flowSites
-## colnames(resultDF) <- paste0("qExc", format(seq(99.9,0.1,-0.1),
-##                                             trim=TRUE, nsmall=01))
+## Result is a single df w/ columns as follows.
+## Site   q0.1% q0.2% q0.3% q0.4% q0.5% ....
+## MC14A  0     0     0     0     0
+## MC17   0.12  0.16  0.16  0.19  0.19
+## MC22A  3.5   3.88  4.14  4.2   4.4
+## .....
 
-## resultDF <- as.data.frame(resultDF)
-## resultDF$siteCode <- rownames(resultDF)
 
 ## Save rds
 saveRDS(resultDF, "USGS-Flow/refFlowQuantiles.rds")
